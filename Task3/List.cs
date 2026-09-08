87ec828db2d02ea13874cadbb80864921d158764
3ofi <alofyomar4@gmail.com>
2026-09-08 18:09:09 +0300

    Add project files.

---
+++ Task3/List.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Task3
+{
+
+    public class GenericListManager<T>
+    {
+        List<T> iteams= new List<T>();
+        List<string> logs= new List<string>();
+        DateTime? LastCreditAt;
+        DateTime? LastSearchAt;
+
+        public void Add(T iteam)
+        {
+            iteams.Add(iteam);
+            LastCreditAt = DateTime.Now;
+            logs.Add("New item added at '2026-07-14 10:30:00'");
+            
+        }
+        public void Edit(Predicate<T> predicate,T UpdatedIteam )
+        {
+            int value = iteams.FindIndex(predicate);
+            if (value != -1) {
+                iteams[value]= UpdatedIteam;
+                logs.Add("Item edited at '2026-07-14 10:35:00'");
+            }
+           
+        }
+        public void Delete(Predicate<T> predicate)
+        {
+            int value = iteams.FindIndex(predicate);
+            if (value != -1)
+            {
+                iteams.RemoveAt(value);
+                logs.Add("Item deleted at '2026-07-14 10:40:00'");
+            }
+        }
+        public T?   Find(Predicate<T> predicate)
+        {
+            LastSearchAt = DateTime.Now;
+
+            foreach (T iteam in iteams)
+            {
+                if (predicate(iteam))
+                {
+                    return  iteam;
+                }
+            }
+            return default;
+        }
+       public  List<T> Where(Predicate<T> predicate)
+        {
+            LastSearchAt = DateTime.Now;
+            List<T> results = new List<T>();
+            for (int i = 0; i < iteams.Count; i++) {
+                if(predicate(iteams[i]))
+                {
+                    results.Add(iteams[i]);
+                }
+
+            }
+            return results;
+        }
+        public int GetCount()
+        {
+            return iteams.Count;
+        }
+        public DateTime? GetLastCreatedAt()
+        {
+            return LastCreditAt;
+        }
+        public DateTime? GetLastSearchAt()
+        {
+            return LastSearchAt;
+        }
+        public List<string> GetLogs()
+        {
+              return logs;
+        }
+
+
+
+    };
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public decimal Salary { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {Name}, Salary: {Salary}";
+        }
+    }
+    public class Client
+    {
+         public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public string Email { get; set; } = "";
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {Name}, Email: {Email}";
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public class Task3
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            bool running = true;
+            while (running)
+            {
+                
+                Console.WriteLine("=== Main Menu ===");
+                Console.WriteLine("1. Employees Screen");
+                Console.WriteLine("2. Clients Screen");
+                Console.WriteLine("3. Exit");
+                Console.Write("Choose an option: ");
+               
+                string choice = Console.ReadLine();
+                
+                switch (choice)
+                {
+                    case "1":
+                        ShowEmployeesScreen();
+                        break;
+                    case "2":
+                        ShowClientsScreen();
+                        break;
+                    case "3":
+                        running = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice, press any key to try again...");
+                        Console.ReadKey();
+                        break;
+                }
+
+            }
+            Console.Clear();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        static void ShowEmployeesScreen() {
+            Console.Clear();
+            // Display the Employees in the list
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("-------------------------Employees------------------------:");
+            Console.ForegroundColor = ConsoleColor.White;
+            var EmployeeManager = new GenericListManager<Employee>();
+            EmployeeManager.Add(new Employee { Id = 1, Name = "Omar Aloufy", Salary = 5500 });
+            EmployeeManager.Add(new Employee { Id = 2, Name = "Omar Shalaby", Salary = 7500 });
+            EmployeeManager.Add(new Employee { Id = 3, Name = "Marwan Ehab", Salary = 9500 });
+            EmployeeManager.Add(new Employee { Id = 4, Name = "Youssef Mahmoud", Salary = 11500 });
+
+
+            Employee? employee = EmployeeManager.Find(e => e.Id == 3);
+            List<Employee> HighSalaryemployees = EmployeeManager.Where(e => e.Salary > 7500);
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("High Salary Employees:");
+            Console.ForegroundColor = ConsoleColor.White;
+            foreach (var emp in HighSalaryemployees)
+            {
+                Console.WriteLine(emp);
+            }
+
+            EmployeeManager.Edit(
+                e => e.Id == 1, new Employee { Id = 1, Name = "Omar Alofy", Salary = 6000 }
+                );
+
+
+            EmployeeManager.Delete(e => e.Id == 4);
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Employee Count:");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(EmployeeManager.GetCount());
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Last Created At:");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(EmployeeManager.GetLastCreatedAt());
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Last Search At:");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(EmployeeManager.GetLastSearchAt());
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Logs:");
+            Console.ForegroundColor = ConsoleColor.White;
+            foreach (var log in EmployeeManager.GetLogs())
+            {
+                Console.WriteLine(log);
+            }
+        }
+
+
+
+
+
+
+
+
+        // Display the Client in the list
+        static void ShowClientsScreen()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("-------------------------Clients------------------------:");
+            var ClientManager = new GenericListManager<Client>();
+            ClientManager.Add(new Client { Id = 1, Name = "Omar Aloufy", Email = "alofyomar511@gmail.com" });
+            ClientManager.Add(new Client { Id = 2, Name = "Omar Shalaby", Email = "omar.shalaby@gmail.com" });
+            ClientManager.Add(new Client { Id = 3, Name = "Marwan Ehab", Email = "marwan.ehab@gmail.com" });
+            ClientManager.Add(new Client { Id = 4, Name = "Youssef Mahmoud", Email = "youssef.mahmoud@gmail.com" });
+           
+            Client? client = ClientManager.Find(c => c.Id == 2);
+            List<Client> HighValueClients = ClientManager.Where(c => c.Id < 3);
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("High Value Clients:");
+            Console.ForegroundColor = ConsoleColor.White;
+            foreach (var emp in HighValueClients)
+            {
+                Console.WriteLine(emp);
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            ClientManager.Edit(
+                c => c.Id == 1, new Client { Id = 1, Name = "Omar Alofy", Email = "alofyomar511@gmail.com" }
+                );
+
+            ClientManager.Delete(c => c.Id == 4);
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Client Count:");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(ClientManager.GetCount());
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Last Created At:");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(ClientManager.GetLastCreatedAt());
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Last Search At:");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(ClientManager.GetLastSearchAt());
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Logs:");
+            Console.ForegroundColor = ConsoleColor.White;
+            foreach (var log in ClientManager.GetLogs())
+            {
+                Console.WriteLine(log);
+            }
+
+        }
+
+
+            
+        
+    }
+}
